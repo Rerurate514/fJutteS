c4cf4055836be47f01c2bb49f517eb87b6c08c7f
Rerurate514 <rerurateyuto@gmail.com>
2025-01-29 18:59:54 +0900

    borderクラスの追加

---
+++ models/border.js
@@ -0,0 +1,24 @@
+class Border {
+    constructor(
+        borderSize = "0px", 
+        borderProperty = "solid",
+        color = "transparent",
+        isTop = true,
+        isLeft = true,
+        isRight = true,
+        isBottom = true
+    ){
+        this.borderSize = borderSize;
+        this.borderProperty = borderProperty;
+        this.color = color;
+
+        this.isTop = isTop;
+        this.isLeft = isLeft;
+        this.isRight = isRight;
+        this.isBottom = isBottom;
+    }
+
+    assembleCSS(){
+        return this.borderSize + " " + this.borderProperty + " " + this.color;
+    }
+}
