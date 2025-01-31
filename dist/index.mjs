9b6a100d1790f605408106ae3b3b4e338ec4459b
Rerurate514 <rerurateyuto@gmail.com>
2025-01-31 19:30:05 +0900

    indexの更新

--- dist/index.mjs
+++ dist/index.mjs
@@ -686,9 +686,39 @@ class Center extends View {
     }
 
     styledView(element){
-        element.style.textAlign = "center";
         element.style.width = "100%";
         element.style.height = "100%";
+
+        element.style.textAlign = "center";
+        element.style.justifyContent = "center";
+
+        element.style.display = "flex";
+        element.style.alignItems = "center";
+
+        return element;
+    }
+
+    build(){
+        return new _Center(this.props.child);
+    }
+}
+
+
+class _Center extends View {
+    constructor(child){
+        super({child: child});
+    }
+
+    createWrapView(){
+        let center = document.createElement("div");
+        return center;
+    }
+
+    styledView(element){
+        element.style.width = "fit-content";
+        element.style.height = "fit-content";
+
+        element.style.margin = "auto";
 
         return element;
     }
