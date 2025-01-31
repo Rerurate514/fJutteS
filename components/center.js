153f435f568359490a63f6a7a8616840579d3313
Rerurate_514 <rerurateyuto@gmail.com>
2025-01-31 13:48:11 +0900

    centerコンポーネントのバグ修正

--- components/center.js
+++ components/center.js
@@ -11,9 +11,31 @@ export class Center extends View {
     }
 
     styledView(element){
-        element.style.textAlign = "center";
         element.style.width = "100%";
         element.style.height = "100%";
+
+        return element;
+    }
+
+    build(){
+        return new _Center(this.props.child);
+    }
+}
+
+
+export class _Center extends View {
+    constructor(child){
+        super({child: child});
+    }
+
+    createWrapView(){
+        let center = document.createElement("div");
+        return center;
+    }
+
+    styledView(element){
+        element.style.textAlign = "center";
+        element.style.justifyContent = "center";
 
         return element;
     }
