d6cc96e099ca2261c3880d1d0e0ccddae7347db3
Rerurate514 <rerurateyuto@gmail.com>
2025-02-01 23:09:56 +0900

    index.mjsのコメントを削除するコードの追加

--- rollup.config.js
+++ rollup.config.js
@@ -9,5 +9,24 @@ export default {
       file: 'dist/index.mjs',
       format: 'es'
     }
+  ],
+  plugins: [
+    removeComments()
   ]
 };
+
+function removeComments() {
+  return {
+    name: 'remove-comments',
+    transform(code) {
+      code = code.replace(/\/\/.*/g, '');
+      code = code.replace(/\/\*[\s\S]*?\*\//g, '');
+      code = code.replace(/^\s*[\r\n]/gm, '');
+      
+      return {
+        code,
+        map: null
+      };
+    }
+  };
+}
