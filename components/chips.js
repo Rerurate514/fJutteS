70e68f9323f887fff81a38bd191a2f9e124216cc
Rerurate514 <rerurateyuto@gmail.com>
2025-02-03 19:08:57 +0900

    Chipsコンポーネントの追加

---
+++ components/chips.js
@@ -0,0 +1,46 @@
+import { View } from "../interface/view.js";
+import { Text } from "./text.js";
+import { Card } from "./card.js";
+import { ShadowLevel } from "../enums/shadowLevel.js";
+import { Border } from "../models/border.js";
+
+export class Chips extends View {
+    constructor({
+        text,
+        background = null,
+        border = new Border()
+    }){
+        super({
+            text: text,
+            background: background,
+            border: border.assembleCSS()
+        });
+    }
+
+    createWrapView(){
+        return document.createElement("div");
+    }
+
+    styledView(element){
+        element.style.borderRadius = "32px";
+
+        element.style.paddingLeft = "16px";
+        element.style.paddingRight = "16px";
+        element.style.border = this.props.border;
+
+        element.style.overflow = "hidden";
+
+        element.style.boxShadow = ShadowLevel.LVL3;
+
+        if(this.props.background) element.style.background = this.props.background;
+
+        return element;
+    }
+
+    build(){
+        return new Card({
+            background: this.props.background ?? "transparent",
+            child: new Text(this.props.text)
+        });
+    }
+}
