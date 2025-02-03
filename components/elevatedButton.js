2a3b9e7020f98d5c7939377cb50756f988a798e5
Rerurate514 <rerurateyuto@gmail.com>
2025-02-03 22:41:40 +0900

    elevatedbuttonにhoverの追加

--- components/elevatedButton.js
+++ components/elevatedButton.js
@@ -1,5 +1,6 @@
 import { BaseCSS } from "../enums/baseCSS.js";
 import { View } from "../interface/view.js";
+import { Hover } from "./hover.js";
 
 export class ElevatedButton extends View {
     constructor({
@@ -19,12 +20,48 @@ export class ElevatedButton extends View {
     }
 
     styledView(element){
-        element = this.props.baseCSS.applyCSS(element);
         return element;
     }
 
     embedScriptToView(element){
         element.addEventListener("click", this.props.onClick);
+
+        return element;
+    }
+
+    build(){
+        return new Hover({
+            child: new _ElevatedButton(this.props)
+        })
+    }
+}
+
+export class _ElevatedButton extends View {
+    constructor({
+        child,
+        baseCSS = new BaseCSS(),
+        onClick: onClick = () => {},
+    }){
+        super({
+            child: child,
+            baseCSS: baseCSS,
+            onClick: onClick
+        });
+    }
+
+    createWrapView(){
+        return document.createElement("div");
+    }
+
+    styledView(element){
+        element.style.paddingLeft = "14px";
+        element.style.paddingRight = "14px";
+
+        element = this.props.baseCSS.applyCSS(element);
+        return element;
+    }
+
+    embedScriptToView(element){
         return element;
     }
 
