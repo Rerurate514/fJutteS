e704cfe6066c1a5da0c569ce2768b6c5af2af10a
Rerurate514 <rerurateyuto@gmail.com>
2025-02-04 23:18:03 +0900

    textFormコンポーネントの追加

---
+++ components/textForm.js
@@ -0,0 +1,63 @@
+import { BaseCSS } from "../enums/baseCSS.js";
+import { View } from "../interface/view.js";
+
+export class TextForm extends View {
+    constructor({
+        provider = null,
+        minLength = null,
+        maxLength = null,
+        value = null,
+        placeholder = "",
+        required = "",
+        pattern = "",
+        
+        baseCSS = new BaseCSS,
+    }){
+        super({
+            provider: provider,
+            minLength: minLength,
+            maxLength: maxLength,
+            value: value,
+            placeholder: placeholder,
+            required: required,
+            pattern: pattern,
+            baseCSS: baseCSS
+        });
+    }
+
+    createWrapView(){
+        const textForm = document.createElement("input");
+        textForm.type = "text";
+        return textForm;
+    }
+
+    styledView(element){
+        element = this.props.baseCSS.applyCSS(element);
+
+        element.value = this.props.value || '';
+        element.placeholder = this.props.placeholder;
+        element.minLength = this.props.minLength;
+        element.maxLength = this.props.maxLength;
+        element.required = this.props.required !== "";
+        element.pattern = this.props.pattern;
+
+        return element;
+    }
+
+    embedScriptToView(element) {
+        element.addEventListener('input', () => {
+            const currentValue = element.value;
+            this.updateProvider(currentValue);
+        });
+        
+        return element;
+    }
+    
+    updateProvider(value) {
+        if (!this.props.provider) return;
+
+        this.props.provider.update(() => {
+            return value
+        });
+    }
+}
