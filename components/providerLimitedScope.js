845c574ed19d15c0259a661600c884819c1ecd57
Rerurate514 <rerurateyuto@gmail.com>
2025-02-10 20:07:55 +0900

    ProviderLimitedScopeコンポーネントの追加

---
+++ components/providerLimitedScope.js
@@ -0,0 +1,41 @@
+import { ProviderScope } from "../interface/providerScope.js";
+
+export class ProviderLimitedScope extends ProviderScope {
+    constructor({
+        props = {}, 
+        watchingProviders = [],
+        build = (value) => {
+            return undefined;
+        }
+    }) {
+        props.providers = watchingProviders;
+        props.build = build;
+
+        props.providers.forEach(provider => {
+            provider.watch(() => {
+                const view = props.build(provider.read())
+                this.rebuild({
+                    builtView: view
+                })
+            },
+            { immediate: false });
+        });
+
+        super({
+            props: props,
+            watchingProviders: props.providers
+        });
+    }
+
+    createWrapView(){
+        return document.createElement("div");
+    }
+
+    build(){
+        const readArr = this.props.providers.map((provider) => {
+            return provider.read();
+        });
+
+        return this.props.build(readArr);
+    }
+}
