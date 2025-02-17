da81643541e180abdd4aaf70916fc2f4ef5a0e12
Rerurate514 <rerurateyuto@gmail.com>
2025-02-17 21:23:18 +0900

    indexの更新

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ export { SimpleDialog  } from './components/simpleDialog.js';
 export { DropDownMenu, DropDownMenuItem } from './components/dropDownMenu.js';
 export { FloatingActionButton } from './components/floatingActionButton.js';
 export { LimitedProviderScope } from './components/limitedProviderScope.js';
+export { ExpandsPanel } from './components/expandsPanel.js';
 
 export { ShadowLevel } from './enums/shadowLevel.js';
 export { TextCSS } from './enums/textCSS.js';
