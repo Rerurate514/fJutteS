e3bae0a3f1bc840ed1d38ee55fa8d148ebeee535
Rerurate514 <rerurateyuto@gmail.com>
2025-02-17 22:39:30 +0900

    ボタンの表示をわかりやすく修正

--- components/expandsPanel.js
+++ components/expandsPanel.js
@@ -1,17 +1,22 @@
 import { BaseCSS } from "../enums/baseCSS.js";
+import { ShadowLevel } from "../enums/shadowLevel.js";
 import { View } from "../interface/view.js";
 import { Provider } from "../jiperes/provider.js";
 import { Box } from "./box.js";
+import { Card } from "./card.js";
 import { Column } from "./column.js";
 import { ElevatedButton } from "./elevatedButton.js";
 import { LimitedProviderScope } from "./limitedProviderScope.js";
 import { Row } from "./row.js";
+import { SpaceBox } from "./spaceBox.js";
 import { Text } from "./text.js";
 export class ExpandsPanel extends View {
     constructor({
         titleItem: titleItem,
         child,
-        panelItemBackGround = "white"
+        radius = "10px",
+        buttonShadowLevel = ShadowLevel.LVL3,
+        panelItemBackGround = "white",
     }){
         const isExpands = Provider.createProvider((ref) => {
             return false;
@@ -21,6 +26,8 @@ export class ExpandsPanel extends View {
             titleItem: titleItem,
             child: child,
             panelItemBackGround: panelItemBackGround,
+            radius: radius,
+            buttonShadowLevel: buttonShadowLevel,
             isExpands: isExpands
         });
     }
@@ -42,25 +49,34 @@ export class ExpandsPanel extends View {
                     isVerticalCenter: true,
                     children: [
                         this.props.titleItem,
-                        new ElevatedButton({
-                            radius: "8px",
-                            baseCSS: new BaseCSS({
-                                padding: "8px",
-                            }),
-                            child: new LimitedProviderScope({
-                                watchingProviders: [ this.props.isExpands ],
-                                build: (isExpand) => {
-                                    const button = isExpand[0] ? "∧" : "∨";
-                                    return new Text(button)
+                        new SpaceBox({
+                            width: "8px",
+                        }),
+                        new Card({
+                            radius: this.props.radius,
+                            elevation: this.props.buttonShadowLevel,
+                            child: new ElevatedButton({
+                                baseCSS: new BaseCSS({
+                                    padding: "8px",
+                                }),
+                                child: new LimitedProviderScope({
+                                    watchingProviders: [ this.props.isExpands ],
+                                    build: (isExpand) => {
+                                        const button = isExpand[0] ? "∧" : "∨";
+                                        return new Text(button);
+                                    }
+                                }),
+                                onClick: () => {
+                                    this.props.isExpands.update((currentValue) => {
+                                        return !currentValue;
+                                    });
                                 }
-                            }),
-                            onClick: () => {
-                                this.props.isExpands.update((currentValue) => {
-                                    return !currentValue;
-                                });
-                            }
+                            })
                         })
                     ]
+                }),
+                new SpaceBox({
+                    height: "8px",
                 }),
                 new LimitedProviderScope({
                     watchingProviders: [ this.props.isExpands ],
