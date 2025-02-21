2fd06fbd6dc4fa3b1657a9e993bf71c8928a03f0
Rerurate_514 <71661237+Rerurate514@users.noreply.github.com>
2025-02-21 09:21:52 +0900

    ClippingTextコンポーネントを追加

---
+++ components/clippingText.js
@@ -0,0 +1,26 @@
+import { View } from "../interface/view.js";
+
+export class ClippingText extends View {
+  cosntructor(child){
+    super({
+      child: child
+    });
+  }
+
+  createWrapView(){
+    return document.createElement("div");
+  }
+
+  styledView(element){
+    element.style.width = "100%";
+    element.style.height = "100%";
+
+    element.style.webkitBackgroundClip = "text";
+
+    return element;
+  }
+
+  build(){
+    return this.props.child;
+  }
+}
