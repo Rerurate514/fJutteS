a823528bddf5b8dbf06264e76fb1c8831e8931ee
Rerurate514 <rerurateyuto@gmail.com>
2025-02-23 23:05:34 +0900

    Cardコンポーネントにて不要なBorderCSSを削除

--- components/card.js
+++ components/card.js
@@ -2,7 +2,6 @@ import { View } from "../interface/view.js";
 import { ShadowLevel } from '../enums/shadowLevel.js';
 import { BaseCSS } from "../enums/baseCSS.js";
 import { WebkitCSS } from "../enums/webkitCSS.js";
-import { BorderCSS } from "../models/borderCSS.js";
 
 export class Card extends View {
     constructor({
@@ -14,15 +13,10 @@ export class Card extends View {
         }),
         background = null,
         elevation = ShadowLevel.LVL0,
-        borderCSS = new BorderCSS(),
         webkitCSS = new WebkitCSS()
     }){
         if(!(elevation instanceof ShadowLevel)){
             throw TypeError("CardコンポーネントのElevationプロパティには、ShadowLevelクラスのみ受け付けています。\n渡された型:" + typeof elevation);
-        }
-
-        if(!(borderCSS instanceof BorderCSS)){
-            throw TypeError("CardコンポーネントのborderCSSプロパティには、BorderCSSクラスのみ受け付けています。\n渡された型:" + typeof elevation);
         }
 
         if(!(webkitCSS instanceof WebkitCSS)){
@@ -35,7 +29,6 @@ export class Card extends View {
             baseCSS: baseCSS,
             background: background,
             elevation: elevation,
-            borderCSS: borderCSS,
             webkitCSS: webkitCSS
         });
     }
@@ -48,10 +41,8 @@ export class Card extends View {
         element.style.borderRadius = this.props.radius;
         if(this.props.background) element.style.background = this.props.background;
         element.style.boxShadow = this.props.elevation;
-        element.style.border = this.props.border;
 
-        element = this.props.baseCSS.applyCSS(element);
-        element = this.props.borderCSS.applyCSS(element)
+        element = this.props.baseCSS.applyCSS(element)
         element = this.props.webkitCSS.applyCSS(element);
 
         return element;
