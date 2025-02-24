23e4b345ae755dc7537a0c2cc4463b3616ce9e3e
Rerurate514 <rerurateyuto@gmail.com>
2025-02-24 23:16:21 +0900

    added test clickable

---
+++ __test__/components/clickable.test.js
@@ -0,0 +1,72 @@
+import { Clickable } from '../../components/clickable.js';
+import { BaseCSS } from '../../enums/baseCSS.js';
+import { extendsWithoutId } from '../testScripts/matcherWithoutId.js';
+import { Moc } from "../testScripts/moc.js";
+import { mocOnClick, onClickMoc } from '../testScripts/onClickMoc.js';
+
+jest.mock('../../enums/baseCSS', () => ({
+    BaseCSS: jest.fn().mockImplementation(() => ({
+      applyCSS: jest.fn(element => element)
+    }))
+  }));
+
+describe('Clickable', () => {
+    let clickable;
+    let moc;
+
+    beforeEach(() => {
+        clickable = new Clickable({
+            child: new Moc(),
+            baseCSS: new BaseCSS(),
+            onClick: () => { }
+        });
+
+        moc = new Moc();
+
+        extendsWithoutId();
+    });
+
+    describe('constructor', () => {
+        test('デフォルト値で初期化できる', () => {
+            const clickable = new Clickable({
+                child: moc
+            });
+
+            expect(clickable.props.child).toBe(moc);
+            expect(clickable.props.baseCSS.applyCSS).toHaveBeenCalled();
+        });
+
+        test('カスタム値で初期化できる', () => {
+            const clickable = new Clickable({
+                child: moc,
+                baseCSS: new BaseCSS(),
+                onClick: onClickMoc
+            });
+
+            expect(clickable.props.child).toBe(moc);
+            expect(clickable.props.baseCSS.applyCSS).toHaveBeenCalled();
+
+            mocOnClick(clickable);
+        });
+    });
+
+    describe('createWrapView', () => {
+        test('div要素を作成する', () => {
+            const element = clickable.createWrapView();
+            expect(element.tagName.toLowerCase()).toBe('div');
+        });
+    });
+
+    describe('styledView', () => {
+        test('基本的なスタイルを適用する', () => {
+            expect(clickable.props.baseCSS.applyCSS).toHaveBeenCalled();
+        });
+
+        describe('build', () => {
+            test('子要素を返す', () => {
+                const boxWithChild = new Clickable({ child: moc });
+                expect(boxWithChild.build()).toBe(moc);
+            });
+        });
+    });
+})
