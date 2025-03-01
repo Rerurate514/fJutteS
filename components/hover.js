0f8ecb3b552e7c77b15d0fc38bbb6bc4cd9a6c19
Rerurate514 <rerurateyuto@gmail.com>
2025-03-01 20:14:19 +0900

    Hoverの仕組みの改善

--- components/hover.js
+++ components/hover.js
@@ -1,5 +1,6 @@
 import { ShadowLevel } from "../enums/shadowLevel.js";
 import { View } from "../interface/view.js";
+import { Row } from "./row.js";
 
 export class Hover extends View {
     constructor({
@@ -23,33 +24,29 @@ export class Hover extends View {
     styledView(element){
         element.style.width = "fit-content";
         element.style.height = "fit-content";
-
         element.style.position = "relative";
-        element.style.background = "transparent";
-
-        element.style.overflow = "hidden";
-
         element.style.borderRadius = this.props.radius;
 
         return element;
     }
 
     build(){
-        return new _Hover(this.props);
+        return new Row({
+            children: [
+                this.props.child,
+                new _Hover(this.props)
+            ]
+        });
     }
 }
 
 class _Hover extends View {
     constructor({
-        child,
         radius = "inherit",
-        shadow = ShadowLevel.LVL5,
         onClickEffect = false
     }){
         super({
-            child,
             radius,
-            shadow,
             onClickEffect
         });
     }
@@ -59,53 +56,39 @@ class _Hover extends View {
     }
 
     styledView(element){
-        element.style.width = "fit-content";
-        element.style.height = "fit-content";
+        element.style.position = "absolute";
+        element.style.top = "0";
+        element.style.left = "0";
+        element.style.width = "100%";
+        element.style.height = "100%";
+        element.style.background = "rgb(0, 0, 0, 0)";
+        element.style.mixBlendMode = "difference";
+        element.style.transition = "background-color 0.4s";
+        element.style.overflow = "hidden";
+        element.style.zIndex = 999;
         element.style.borderRadius = this.props.radius;
-        element.style.background = "transparent";
 
         return element;
     }
 
-    createAfterElement(){
-        let after = document.createElement("div");
-
-        after.content = "";
-        after.style.position = "absolute";
-        after.style.top = "0";
-        after.style.left = "0";
-        after.style.background = "rgb(0, 0, 0, 0)";
-        after.style.mixBlendMode = "difference";
-        after.style.transition = "background-color 0.4s";
-
-        after.style.overflow = "hidden";
-
-        after.style.zIndex = 999;
-
-        after.style.borderRadius = (parseInt(this.props.radius, 10)) + "px";
-
-        after.style.width = "100%";
-        after.style.height = "100%";
-
-        after.addEventListener('mouseenter', () => {
-            after.style.background = "rgb(100, 100, 100, 0.4)";
-            this.entered = true;
+    embedScriptToView(element) {
+        element.addEventListener('mouseenter', () => {
+            element.style.background = "rgb(100, 100, 100, 0.4)";
         });
 
-        after.addEventListener('mouseleave', () => {
-            after.style.background = "rgb(0, 0, 0, 0)";
-            this.entered = false;
+        element.addEventListener('mouseleave', () => {
+            element.style.background = "rgb(0, 0, 0, 0)";
         });
 
-        if(!this.props.onClickEffect) return after;
+        if(!this.props.onClickEffect) return element;
 
-        after.addEventListener('click', (e) => {
-            const ripples = after.getElementsByClassName('ripple');
+        element.addEventListener('click', (e) => {
+            const ripples = element.getElementsByClassName('ripple');
             Array.from(ripples).forEach(ripple => ripple.remove());
 
             const ripple = document.createElement("div");
             ripple.classList.add('ripple');
-            
+
             ripple.style.position = "absolute";
             ripple.style.transform = "scale(0)";
             ripple.style.animation = "";
@@ -113,35 +96,31 @@ class _Hover extends View {
             ripple.style.background = "rgba(200, 200, 200, 0.9)";
             ripple.style.borderRadius = "50%";
             ripple.style.mixBlendMode = "difference";
-            
-            const rect = after.getBoundingClientRect();
+
+            const rect = element.getBoundingClientRect();
             const x = e.clientX - rect.left;
             const y = e.clientY - rect.top;
 
-            const w = after.offsetWidth;
-            const h = after.offsetHeight;
+            const w = element.offsetWidth;
+            const h = element.offsetHeight;
             const dia = Math.sqrt(w * w + h * h);
 
             ripple.style.width = ripple.style.height = dia * 2 + 'px';
             ripple.style.left = x - dia + 'px';
             ripple.style.top = y - dia + 'px';
 
-            after.appendChild(ripple);
+            element.appendChild(ripple);
 
             ripple.animate([
                 { transform: "scale(1)", opacity: "0"}
             ], {
                 duration: 600,
                 easing: "ease-in-out"
-            })
+            });
 
             setTimeout(() => ripple.remove(), 1000);
         });
 
-        return after;
-    }
-
-    build(){
-        return this.props.child;
+        return element;
     }
 }
